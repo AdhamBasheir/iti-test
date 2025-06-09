cd64631676c5f6182ac6eacbe9c31bee5a677dfc
Adham Basheir <adhambasheir000@gmail.com>
2025-06-09 15:37:47 +0300

    m

--- terraform/infra/modules/eks/outputs.tf
+++ terraform/infra/modules/eks/outputs.tf
@@ -22,16 +22,16 @@ output "eks_oidc_arn" {
 ########################################
 
 
-# data "aws_eks_cluster_auth" "this" {
-#   name = aws_eks_cluster.this.name
-# }
+data "aws_eks_cluster_auth" "this" {
+  name = aws_eks_cluster.this.name
+}
 
-# output "cluster_endpoint" {
-#   value = aws_eks_cluster.this.endpoint
-# }
-# output "cluster_certificate_authority_data" {
-#   value = aws_eks_cluster.this.certificate_authority[0].data
-# }
-# output "cluster_token" {
-#   value = data.aws_eks_cluster_auth.this.token
-# }
+output "cluster_endpoint" {
+  value = aws_eks_cluster.this.endpoint
+}
+output "cluster_certificate_authority_data" {
+  value = aws_eks_cluster.this.certificate_authority[0].data
+}
+output "cluster_token" {
+  value = data.aws_eks_cluster_auth.this.token
+}
